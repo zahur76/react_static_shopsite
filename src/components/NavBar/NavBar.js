f4772cc858c2c70da44e30b687d0165146b890f5
zahur <zahurmeerun@hotmail.com>
2021-11-11 22:03:56 +0400

    incoporate boostrap navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,18 +3,28 @@ import './NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-
+import Navbar from 'react-bootstrap/esm/Navbar';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import Nav from 'react-bootstrap/esm/Nav'
+import { NavDropdown } from 'react-bootstrap';
 class NavBar extends React.Component {
     render(){
         return (
-            <div className='NavBar'>
-                <Row className='row'>
-                    <Col xs='0' md='3'></Col>
-                    <Col xs='9' md='6'><a href="#" className='heading'>Shop All Day</a></Col>
-                    <Col xs='3' md='3'>Contact Us</Col>
-                </Row>
-            </div>
+            <Navbar collapseOnSelect expand="lg" className='custom-nav' variant="dark">
+                <Container>
+                    <Navbar.Brand className="logo" href="#home">Shop All Day</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav>
+                            <Nav.Link className="links" href="#deets">Contact Us</Nav.Link>
+                            <Nav.Link className="links" eventKey={2} href="#memes">
+                                Links
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         )
     }
 }
