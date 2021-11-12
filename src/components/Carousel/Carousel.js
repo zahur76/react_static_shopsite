7c0bd1d35f6fcd3ea081af84362b55722ce02f61
zahur <zahurmeerun@hotmail.com>
2021-11-12 20:25:26 +0400

    simplify carousel to include iteration of objects

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,14 +1,27 @@
-import image from './hoodie.jpg'
-import dress from './dress.jpg'
-import jacket from './jacket.jpg'
 import React from 'react';
 import './Carousel.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import offers from './offers.js'
 
 
 class CarouselDeals extends React.Component {
+    renderOffers(){
+        return Object.keys(offers).map(offer => {
+          let offerValue = offers[offer];
+          return <Carousel.Item interval={1000}>
+                    <img className="d-block" src={offerValue.image} alt="First slide"/>
+                        <Carousel.Caption>
+                                    <h3>
+                                        {offerValue.name}{'\n'} 
+                                        ${offerValue.price}
+                                    </h3>
+                                                        
+                        </Carousel.Caption>
+                </Carousel.Item>
+        });        
+    }    
     render(){
         return (
             <div>
@@ -16,36 +29,7 @@ class CarouselDeals extends React.Component {
                 <Row className="carousel-container">
                     <Col xs={7} sm={5} md={4} lg={3}>
                         <Carousel className="carousel">                
-                            <Carousel.Item interval={1000}>
-                                <img
-                                className="d-block"
-                                src={image}
-                                alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                <h3>Red Hoodie</h3>                    
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item interval={500}>
-                                <img
-                                className="d-block"
-                                src={dress}
-                                alt="Second slide"
-                                />
-                                <Carousel.Caption>
-                                <h3>Summer Dress</h3>                    
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src={jacket}
-                                alt="Third slide"
-                                />
-                                <Carousel.Caption>
-                                <h3>Raincoat</h3>                    
-                                </Carousel.Caption>
-                            </Carousel.Item>
+                            {this.renderOffers()}
                         </Carousel>
                     </Col>
                 </Row>
---
+++ src/components/Carousel/offers.js
@@ -0,0 +1,24 @@
+import hoodie from './hoodie.jpg'
+import dress from './dress.jpg'
+import jacket from './jacket.jpg'
+
+
+const offers = {
+    hoodie: {
+      image: hoodie,
+      name: 'Red Hoodie',
+      price: 18
+    },
+    dress: {
+      image: dress,
+      name: 'Casual Dress',
+      price: 22
+    },
+    jacket: {
+      image: jacket,
+      name: 'Rain Coat',
+      price: 12
+    }
+};
+
+export default offers;
