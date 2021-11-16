c1f9240acfa243a9cad362a7eca08ac5a955c5f5
zahur <zahurmeerun@hotmail.com>
2021-11-16 18:20:04 +0400

    add setstate to gif on hero image to make dissapear

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -8,12 +8,23 @@ import image from './heroImage.jpg';
 
 
 class HeroImage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { show: "block" }
+        this.removeGif = this.removeGif.bind(this);
+    }
+    removeGif(){
+        const timer = setTimeout(() => {        
+            this.setState({ show: "none"});
+        }, 5000);
+      }   
     render(){
-        return (
+        this.removeGif()
+        return (            
             <Row>
                 <Col xs={12} className='HeroImage'> 
                     <img src={image}/>
-                    <img className="confetti" src='https://i.gifer.com/2ii9.gif'/>                                                                 
+                    <img style={{display: this.state.show}} className="confetti" src='https://i.gifer.com/2ii9.gif'/>                                                                 
                 </Col>
                 <Col className="search-bar" xs={12}>
                     <Form>
