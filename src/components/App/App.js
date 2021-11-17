ec2770de63b153d69f66afff0fa099e08b18ce24
zahur <zahurmeerun@hotmail.com>
2021-11-17 15:56:24 +0400

    add menu component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import HeroImage from '../HeroImage/HeroImage'
 import CarouselDeals from '../Carousel/Carousel.js';
 import Departments from '../Departments/Departments.js';
 import Footer from '../Footer/Footer.js'
+import Menu from '../Menu/Menu.js'
 
 function App() {
   return (
     <div className="App">
       <NavBar title="Shop All Day"/>
-      <HeroImage />      
+      <HeroImage /> 
+      <Menu />     
       <CarouselDeals />
       <Departments />
       <Footer />       
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,2 +1,51 @@
 import React from 'react';
-import './Menus.css';
+import './Menu.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const menus = {
+    all: 'All',
+    men : "Men",
+    women: "Women",
+    kids: "kids"
+}
+
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        // set current state of searchbar/Dynamnic values
+        this.state = {sortBy: 'All'};        
+    }
+
+    getSortByClass(menu){
+        if(menu===this.state.sortBy){
+            return 'menu active'
+        }else {
+            return 'menu'
+        }
+    }
+
+    handleSortByChange(menuValue){
+        this.setState({ sortBy: menuValue})
+    }
+
+    renderMenu(){
+        return Object.keys(menus).map(menu=>{
+            let menuValue = menus[menu];
+            return <Col><a href="#" onClick={this.handleSortByChange.bind(this, menuValue)} className={this.getSortByClass(menuValue)}>{menuValue}</a></Col>
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <Row>
+                    {this.renderMenu()}
+                </Row>
+            </div>
+        )        
+    }
+}
+
+export default Menu
